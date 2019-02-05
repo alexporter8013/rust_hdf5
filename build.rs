c835e4929060d4787bf045bbb956edfceb057c97
Alex Porter <bomber8013@gmail.com>
2019-02-04 19:44:14 -0800

    changed build script

--- build.rs
+++ build.rs
@@ -1,4 +1,19 @@
+use std::env;
+
 fn main() {
 
 	println!("Hello, build!");
+	let key = "HDF5_INCLUDE";
+	match env::var_os(key) {
+	    Some(val) => println!("{}: {:?}", key, val),
+	    None => println!("{} is not defined in the environment.", key)
+	}
+	let key = "HDF5_LIB";
+	match env::var_os(key) {
+	    Some(val) => println!("{}: {:?}", key, val),
+	    None => println!("{} is not defined in the environment.", key)
+	}
+	for (key, value) in env::vars() {
+    	println!("{}: {}", key, value);
+	}
 }
